58b37430d51c654774f5133560885821c78920a6
Pietrek14 <dawidekpe@gmail.com>
2023-04-02 00:21:55 +0200

    filled in the game's name and fixed the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
 mod actions;
-mod audio;
 mod loading;
 mod menu;
-mod player;
 
 use crate::actions::ActionsPlugin;
-use crate::audio::InternalAudioPlugin;
 use crate::loading::LoadingPlugin;
 use crate::menu::MenuPlugin;
-use crate::player::PlayerPlugin;
 
 use bevy::app::App;
 #[cfg(debug_assertions)]
@@ -36,9 +32,7 @@ impl Plugin for GamePlugin {
         app.add_state::<GameState>()
             .add_plugin(LoadingPlugin)
             .add_plugin(MenuPlugin)
-            .add_plugin(ActionsPlugin)
-            .add_plugin(InternalAudioPlugin)
-            .add_plugin(PlayerPlugin);
+            .add_plugin(ActionsPlugin);
 
         #[cfg(debug_assertions)]
         {
