6a50ada98c9284f16353521a498d562c711dd723
imustend <plochockitymon@gmail.com>
2023-04-09 00:20:57 +0200

    camera

--- src/camera.rs
+++ src/camera.rs
@@ -17,119 +17,15 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_plugin(Material2dPlugin::<DefaultPostMaterial>::default())
-        .add_system(camera_setup.in_schedule(OnEnter(GameState::Menu)));
+        .add_system(camera_setup.in_schedule(OnEnter(GameState::Loading)));
         
-    }
-}
-
-#[derive(AsBindGroup, TypeUuid, Clone)]
-#[uuid = "812b0af1-b5a1-47e2-bf36-45eb612fadc1"]
-struct DefaultPostMaterial {
-    #[texture(0)]
-    #[sampler(1)]
-    source_image: Handle<Image>,
-}
-
-impl Material2d for DefaultPostMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/default.wgsl".into()
     }
 }
 
 fn camera_setup(
     mut commands: Commands,
-    windows: Query<&Window>,
-    mut images: ResMut<Assets<Image>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut post_processing_materials: ResMut<Assets<DefaultPostMaterial>>,
 ) {
     // This assumes we only have a single window
-    let window = windows.single();
+    commands.spawn(Camera2dBundle::default());
 
-    let size = Extent3d {
-        width: window.resolution.physical_width(),
-        height: window.resolution.physical_height(),
-        ..default()
-    };
-
-    // This is the texture that will be rendered to.
-    let mut image = Image {
-        texture_descriptor: TextureDescriptor {
-            label: None,
-            size,
-            dimension: TextureDimension::D2,
-            format: TextureFormat::bevy_default(),
-            mip_level_count: 1,
-            sample_count: 1,
-            usage: TextureUsages::TEXTURE_BINDING
-                | TextureUsages::COPY_DST
-                | TextureUsages::RENDER_ATTACHMENT,
-            view_formats: &[],
-        },
-        ..default()
-    };
-
-    // fill image.data with zeroes
-    image.resize(size);
-
-    let image_handle = images.add(image);
-
-    // Main camera, first to render
-    commands.spawn((
-        Camera2dBundle {
-            camera_2d: Camera2d {
-                clear_color: ClearColorConfig::Custom(Color::WHITE),
-                ..default()
-            },
-            camera: Camera {
-                target: RenderTarget::Image(image_handle.clone()),
-                ..default()
-            },
-            ..default()
-        },
-        // Disable UI rendering for the first pass camera. This prevents double rendering of UI at
-        // the cost of rendering the UI without any post processing effects.
-        UiCameraConfig { show_ui: false },
-    ));
-
-    // This specifies the layer used for the post processing camera, which will be attached to the post processing camera and 2d quad.
-    let post_processing_pass_layer = RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8);
-
-    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
-        size.width as f32,
-        size.height as f32,
-    ))));
-
-    // This material has the texture that has been rendered.
-    let material_handle = post_processing_materials.add(DefaultPostMaterial {
-        source_image: image_handle,
-    });
-
-    // Post processing 2d quad, with material using the render texture done by the main camera, with a custom shader.
-    commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: quad_handle.into(),
-            material: material_handle,
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 1.5),
-                ..default()
-            },
-            ..default()
-        },
-        post_processing_pass_layer,
-    ));
-
-    // The post-processing pass camera.
-    commands.spawn((
-        Camera2dBundle {
-            camera: Camera {
-                // renders after the first main camera which has default value: 0.
-                order: 1,
-                ..default()
-            },
-            ..Camera2dBundle::default()
-        },
-        post_processing_pass_layer,
-    ));
 }
