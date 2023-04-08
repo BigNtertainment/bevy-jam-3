f7b2e748f7d9f3ddcc111a3d848aacbed6429960
imustend <plochockitymon@gmail.com>
2023-04-09 00:22:29 +0200

    ds

--- src/camera.rs
+++ src/camera.rs
@@ -1,14 +1,4 @@
-use bevy::{
-    prelude::*,
-    render::{
-        render_resource::{
-            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages, AsBindGroup, ShaderRef,
-        },
-        texture::BevyDefault,
-        view::RenderLayers, camera::RenderTarget,
-    },
-    sprite::{Material2dPlugin, Material2d, MaterialMesh2dBundle}, reflect::TypeUuid, core_pipeline::clear_color::ClearColorConfig,
-};
+use bevy::prelude::*;
 
 use crate::GameState;
 
@@ -16,16 +6,11 @@ pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app
-        .add_system(camera_setup.in_schedule(OnEnter(GameState::Loading)));
-        
+        app.add_system(camera_setup.in_schedule(OnEnter(GameState::Loading)));
     }
 }
 
-fn camera_setup(
-    mut commands: Commands,
-) {
+fn camera_setup(mut commands: Commands) {
     // This assumes we only have a single window
     commands.spawn(Camera2dBundle::default());
-
 }
